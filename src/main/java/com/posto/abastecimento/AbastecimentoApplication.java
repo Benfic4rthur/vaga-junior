9e0e12f85a2624daf78807b2a7da7a25f8a36ba0
Arthur Benfica Graff <arthur_benfica@hotmail.com>
2026-09-25 18:53:54 -0300

    Adiciona inicialização da aplicação

---
+++ src/main/java/com/posto/abastecimento/AbastecimentoApplication.java
@@ -0,0 +1,12 @@
+package com.posto.abastecimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbastecimentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbastecimentoApplication.class, args);
+    }
+}
