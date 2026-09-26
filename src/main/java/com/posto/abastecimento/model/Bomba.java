902139a7c75b7b6d9b3680e603af8596b1bb268b
Arthur Benfica Graff <arthur_benfica@hotmail.com>
2026-09-26 13:10:11 -0300

    Cria entidade de bomba

---
+++ src/main/java/com/posto/abastecimento/model/Bomba.java
@@ -0,0 +1,46 @@
+package com.posto.abastecimento.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Bomba {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "tipo_combustivel_id", nullable = false)
+    private TipoCombustivel tipoCombustivel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public TipoCombustivel getTipoCombustivel() {
+        return tipoCombustivel;
+    }
+
+    public void setTipoCombustivel(TipoCombustivel tipoCombustivel) {
+        this.tipoCombustivel = tipoCombustivel;
+    }
+}
